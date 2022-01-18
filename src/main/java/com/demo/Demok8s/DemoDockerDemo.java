d8371f0a1bd517549c0a9ef00be7f0198ea4f078
rohittera7 <rohittera7@gmail.com>
2022-01-18 12:42:46 -0800

    added all files

---
+++ src/main/java/com/demo/Demok8s/DemoDockerDemo.java
@@ -0,0 +1,13 @@
+package com.demo.Demok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerDemo.class, args);
+    }
+
+}
---
+++ src/main/java/com/demo/Demok8s/ws/DemoDockerDemoController.java
@@ -0,0 +1,13 @@
+package com.demo.Demok8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoDockerDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World - Demo-K8s";
+    }
+}
