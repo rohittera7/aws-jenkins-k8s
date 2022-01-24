a90b54dc8fea243d891221ecce2cb101bbf03c0c
rohittera7 <rohittera7@gmail.com>
2022-01-23 22:47:29 -0800

    updated message

--- src/main/java/com/demo/Demok8s/ws/DemoDockerDemoController.java
+++ src/main/java/com/demo/Demok8s/ws/DemoDockerDemoController.java
@@ -8,6 +8,6 @@ public class DemoDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World - Welcome Demo - aampe";
+        return "Hello Aampe - Welcome to Demo";
     }
 }
